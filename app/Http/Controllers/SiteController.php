8092ca69b1d6d9b4e657305a6bb02ea76b951c61
Ashok812 <chhetriarash81@gmail.com>
2021-06-11 11:40:36 +0545

    Admin and Site Controller Created

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function home()
+    {
+        if(auth()->user()->role != 'Admin')
+            return redirect('/');
+
+        return redirect('/admin');
+    }
+
+}
