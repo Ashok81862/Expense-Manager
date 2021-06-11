481b6835281d69718601091e7ee4d88736fa22b4
Ashok812 <chhetriarash81@gmail.com>
2021-06-11 18:59:13 +0545

    Income Model and Migration Created

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Income extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function incomeCategory()
+    {
+        return $this->belongsTo(IncomeCategory::class);
+    }
 }
--- database/migrations/2021_06_11_131108_create_incomes_table.php
+++ database/migrations/2021_06_11_131108_create_incomes_table.php
@@ -15,6 +15,9 @@ class CreateIncomesTable extends Migration
     {
         Schema::create('incomes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('income_category_id');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
