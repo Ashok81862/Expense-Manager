f8afac5787119c2356ee3f3b5e1b799b56f8ec0f
Ashok812 <chhetriarash81@gmail.com>
2021-06-11 13:51:58 +0545

    Expense Model Created

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function expenseCategory()
+    {
+        return $this->belongsTo(ExpenseCategory::class);
+    }
 }
