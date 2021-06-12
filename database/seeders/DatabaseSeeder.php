6cece90157f688b20bc396acce1484afb3773591
Ashok812 <chhetriarash81@gmail.com>
2021-06-12 13:27:24 +0545

    Income Factory Created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Expense;
 use App\Models\ExpenseCategory;
+use App\Models\IncomeCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(ExpenseCategorySeeder::class);
         $this->call(ExpenseSeeder::class);
+        $this->call(IncomeCategorySeeder::class);
+        $this->call(IncomeSeeder::class);
     }
 }
