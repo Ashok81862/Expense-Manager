626d698d8b1e1979ef8fa29ea7c7680a48cdc9d2
Ashok812 <chhetriarash81@gmail.com>
2021-06-12 22:24:12 +0545

    IncomeCategory Api Created

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\AuthController;
+use App\Http\Controllers\Api\V1\IncomeCategoryController;
 use App\Http\Controllers\API\V1\ExpenseCategoryController;
 
 Route::prefix('v1')->group(function () {
@@ -17,17 +18,26 @@ Route::prefix('v1')->group(function () {
 
         Route::get('user',[AuthController::class, 'user']);
 
+        //ExpenseCategory Routes
         Route::get('expenseCategories', [ExpenseCategoryController::class, 'index']);
         Route::post('expenseCategory', [ExpenseCategoryController::class, 'store']);
         Route::get('/expenseCategories/{expenseCategory}', [ExpenseCategoryController::class, 'show']);
         Route::put('/expenseCategories/{expenseCategory}', [ExpenseCategoryController::class, 'update']);
         Route::delete('/expenseCategories/{expenseCategory}', [ExpenseCategoryController::class, 'destroy']);
 
+        //Expense Routes
         Route::get('expenses',[\App\Http\Controllers\API\V1\ExpenseController::class,'index']);
         Route::post('expense',[\App\Http\Controllers\API\V1\ExpenseController::class,'store']);
         Route::get('/expenses/{expense}',[\App\Http\Controllers\API\V1\ExpenseController::class,'show']);
         Route::put('/expenses/{expense}',[\App\Http\Controllers\API\V1\ExpenseController::class,'update']);
         Route::delete('/expenses/{expense}',[\App\Http\Controllers\API\V1\ExpenseController::class,'destroy']);
+
+        //IncomeCategory Routes
+        Route::get('incomeCategories', [IncomeCategoryController::class, 'index']);
+        Route::post('incomeCategory', [IncomeCategoryController::class, 'store']);
+        Route::get('/incomeCategories/{incomeCategory}', [IncomeCategoryController::class, 'show']);
+        Route::put('/incomeCategories/{incomeCategory}', [IncomeCategoryController::class, 'update']);
+        Route::delete('/incomeCategories/{incomeCategory}', [IncomeCategoryController::class, 'destroy']);
 
 
     });
