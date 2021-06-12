505987788541eb32a9ac98aeffed86cfddfe40f7
Ashok812 <chhetriarash81@gmail.com>
2021-06-12 18:20:55 +0545

    Api Route Created

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+require_once __DIR__ . "/api/v1.php";
+
